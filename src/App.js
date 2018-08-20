b525fd43607621037568030dca8556b2f9dabd50
Viktor Dushevski <exektc@gmail.com>
2018-08-20 13:00:35 +0300

    add base routing and pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Provider } from "react-redux";
 import store from "./store";
-
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import Message from "./components/Message";
+
+import IndexPage from "./pages";
+import LoginPage from "./pages/Login";
+import NewsPage from "./pages/News";
+import ProfilePage from "./pages/Profile";
 
 const App = () => (
   <Provider store={store}>
-    <Message />
+    <BrowserRouter>
+      <Fragment>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+
+        <Route exact path="/" component={IndexPage} />
+        <Route path="/news" component={NewsPage} />
+        <Route path="/profile" component={ProfilePage} />
+        <Route path="/login" component={LoginPage} />
+        <Message />
+      </Fragment>
+    </BrowserRouter>
   </Provider>
 );
 
