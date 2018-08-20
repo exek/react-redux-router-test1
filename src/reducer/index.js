2a110b71a97b507748e7a3fe13b329185b0bdc63
Viktor Dushevski <exektc@gmail.com>
2018-08-20 16:32:46 +0300

    add auth reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
 import news from "./news";
+import auth from "./auth";
 
 const message = (state = "Hello from redux store", action) => {
   return state;
 };
 
 export default combineReducers({
+  auth,
   message,
   news
 });
