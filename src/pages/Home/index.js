23a501c0152cbaf88176d277c97864e8f1662a08
Viktor Dushevski <exektc@gmail.com>
2018-08-20 16:34:06 +0300

    refactor pages

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+Index.propTypes = {};
+
+export default Index;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Route, Link } from "react-router-dom";
+
+import LoginPage from "./Login";
+import NewsPage from "./News";
+import ProfilePage from "./Profile";
+import HomePage from "./Home";
 
 const Index = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Fragment>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/news">News</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+
+      <Route path="/news" component={HomePage} />
+      <Route path="/news" component={NewsPage} />
+      <Route path="/profile" component={ProfilePage} />
+      <Route path="/login" component={LoginPage} />
+    </Fragment>
   );
 };
 
