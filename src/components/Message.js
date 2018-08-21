fe04d8ab0967d8691f0b5ef195263c45139b8526
Viktor Dushevski <exektc@gmail.com>
2018-08-21 09:25:35 +0300

    remove dummy Message component

--- src/components/Message.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { connect } from "react-redux";
-
-const Message = props => <h1>{props.message}</h1>;
-
-export default connect(state => ({
-  message: state.message
-}))(Message);
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,12 +2,7 @@ import { combineReducers } from "redux";
 import news from "./news";
 import auth from "./auth";
 
-const message = (state = "Hello from redux store", action) => {
-  return state;
-};
-
 export default combineReducers({
   auth,
-  message,
   news
 });
