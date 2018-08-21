0ceb580529e34b48a09e019c02a5e8beae2bc0ba
Viktor Dushevski <exektc@gmail.com>
2018-08-21 15:54:52 +0300

    update profile page

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Login = () => {
+const Login = props => {
   return (
     <div>
       <h1>Profile Page</h1>
+      <h2>Hello {props.user}</h2>
     </div>
   );
 };
 
 Login.propTypes = {};
 
-export default Login;
+const mapStateToProps = state => ({
+  user: state.auth.user
+});
+
+export default connect(mapStateToProps)(Login);
