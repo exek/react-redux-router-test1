45ff9974ffcf1aec82e4019f5ecd4f28411d44ba
Viktor Dushevski <exektc@gmail.com>
2018-08-22 11:46:39 +0300

    add localStorage persisted state

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,24 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import reducer from "../reducer";
+import { loadState, saveState } from "./localStorage";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const persistedState = loadState();
+
+const store = createStore(
+  reducer,
+  persistedState,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+store.subscribe(() =>
+  saveState({
+    auth: {
+      user: store.getState().auth.user
+    }
+  })
+);
 
 export default store;
