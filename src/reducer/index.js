64c7fd922d4fd6bb606a019baf1b4455ca734fa4
Viktor Dushevski <exektc@gmail.com>
2018-08-22 15:01:08 +0300

    add user reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import news from "./news";
 import auth from "./auth";
+import user from "./user";
 
 export default combineReducers({
   auth,
-  news
+  news,
+  user
 });
---
+++ src/reducer/user.js
@@ -0,0 +1,20 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const init = {
+  user: null,
+  loading: false,
+  error: null
+};
+
+export default (state = init, action) => {
+  switch (action.type) {
+    case actionTypes.USER_FETCH_REQUEST:
+      return { ...init, loading: true };
+    case actionTypes.USER_FETCH_SUCCESS:
+      return { ...init, user: action.user };
+    case actionTypes.USER_FETCH_FAIL:
+      return { ...init, error: action.error };
+    default:
+      return state;
+  }
+};
