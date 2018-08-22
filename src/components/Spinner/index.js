c2e90956dc39138ffbaa99fbeacf6fb6ce41c7c1
Viktor Dushevski <exektc@gmail.com>
2018-08-22 15:44:12 +0300

    add spinner component

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="card" style={{ width: "100%" }}>
+      <div className="card-body text-center">
+        <i className="fa fa-spinner fa-spin" style={{ fontSize: 50 }} />
+      </div>
+    </div>
+  );
+};
